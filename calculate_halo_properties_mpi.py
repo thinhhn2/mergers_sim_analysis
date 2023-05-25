eb52c397e9109dd7afebbf53b35c966f5cfa9c71
thinhhn2 <thinhnh3009@gmail.com>
2023-05-25 12:22:20 -0500

    Return a dictionary instead of a list
    Change the output format of the properties_of_halo function from list to dictionary

--- calculate_halo_properties_mpi.py
+++ calculate_halo_properties_mpi.py
@@ -87,13 +87,16 @@ def properties_of_halo(halo, sim_data):
     sfr = np.sum(s_mass_each[formation_time > currenttime - sf_timescale])/sf_timescale 
     sfr = sfr.in_units('Msun/yr')
     
-    #Calculate the mvir mass from yt
-    mvir = dm_mass + pop2_mass + pop3_mass + g_mass
+    #Calculate the mvir total mass from yt
+    mvir = np.sum(reg["all", "particle_mass"].in_units("Msun")) + g_mass
     
     #Calculate the gas mass fraction
     g_mass_fraction = g_mass/mvir
     
-    return tree_loc, coor,float(currenttime),float(g_mass_fraction), float(g_mass), float(dm_mass), float(h2_mass), float(h2_fraction), float(pop2_mass), float(pop3_mass), float(sm_mass), float(sfr), float(mvir)
+    #Make a dictionary for the output
+    output_dict = {"tree_loc":tree_loc,'coor':coor,'time':float(currenttime),'gas_mass': float(g_mass), 'gas_mass_frac': float(g_mass_fraction),'dm_mass': float(dm_mass), 'h2_mass': float(h2_mass), 'h2_frac':float(h2_fraction),'pop2_mass': float(pop2_mass), 'pop3_mass': float(pop3_mass), 'star_mass': float(sm_mass), 'sfr': float(sfr), 'total_mass':float(mvir)}
+    
+    return output_dict
 
 #----------------------------------------------------------------------------------
 shielding_on = 1
