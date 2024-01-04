94c42a65c3c3cab16f95983ac840d2e781b6ec1a
thinhhn2 <thinhnh3009@gmail.com>
2024-01-04 14:44:16 +0700

    Add AREPO and GIZMO to the code

--- extract_particle_metadata.py
+++ extract_particle_metadata.py
@@ -2,6 +2,7 @@ import yt
 import numpy as np
 from yt.data_objects.particle_filters import add_particle_filter
 import matplotlib.pyplot as plt
+import sys
 
 yt.enable_parallelism()
 from mpi4py import MPI
@@ -9,15 +10,22 @@ comm = MPI.COMM_WORLD
 rank = comm.rank
 nprocs = comm.size
 
-
+#-------------------------------------------------------------------------------------------
+#LOAD DATA
 def stars(pfilter, data):
     filter_stars = np.logical_and(data["all", "particle_type"] == 2, data["all", "particle_mass"].to('Msun') > 1)
     return filter_stars
 
-tree = np.load('halotree_Thinh_structure_with_com.npy',allow_pickle=True).tolist()
+tree_name = sys.argv[1] #for example 'halotree_Thinh_structure_with_com.npy'
+code_name = sys.argv[2] #Select among ENZO, GADGET3, AREPO, GIZMO etc.
+start_idx = int(sys.argv[3]) #if we want to start from a specific snapshot (when restarting, for example)
+
+tree = np.load(tree_name,allow_pickle=True).tolist()
 pfs = np.loadtxt('pfs_manual.dat',dtype='str')
-snapshot_idx = list(tree['0'].keys())
-code_name = 'ENZO' #Select among ENZO, GADGET3, etc.
+snapshot_idx = list(tree['0'].keys())[start_idx:]
+
+#-------------------------------------------------------------------------------------------
+#MAIN CODE
 
 my_storage = {}
 for sto, idx in yt.parallel_objects(snapshot_idx, nprocs-1,storage = my_storage):
@@ -59,8 +67,32 @@ for sto, idx in yt.parallel_objects(snapshot_idx, nprocs-1,storage = my_storage)
         g_coor_each = np.array(g_coor_each)
         g_vel_each = np.array(g_vel_each)
 
-    if code_name == 'GADGET3':
+    if code_name == 'GADGET3' or code_name == 'AREPO':
         ds = yt.load(pfs[int(idx)],unit_base = {"length": (1.0, "Mpccm/h")})
+
+        coor = tree['0'][idx]['coor']
+        rvir = tree['0'][idx]['Rvir']
+
+        reg = ds.sphere(coor,(rvir,'code_length'))
+
+        com_coor_star = reg.quantities.center_of_mass(use_gas = False, use_particles = True, particle_type='PartType4').to('kpc').v
+        com_vel_star = reg.quantities.bulk_velocity(use_gas = False, use_particles = True, particle_type='PartType4').to('km/s').v
+
+        com_coor_bary = reg.quantities.center_of_mass(use_gas = True, use_particles = True, particle_type='PartType4').to('kpc').v
+        com_vel_bary = reg.quantities.bulk_velocity(use_gas = True, use_particles = True, particle_type='PartType4').to('km/s').v
+
+        #Calculating stars' metadata
+        s_mass_each = reg[("PartType4", "particle_mass")].in_units("Msun").v.tolist()
+        s_coor_each = reg[("PartType4", "particle_position")].in_units("kpc").v.tolist()
+        s_vel_each = reg[("PartType4", "particle_velocity")].in_units("km/s").v.tolist()
+
+        #Calculating gas' metadata
+        g_mass_each = reg[("PartType0", "particle_mass")].in_units("Msun").v.tolist()
+        g_coor_each = reg[("PartType0", "particle_position")].in_units("kpc").v.tolist()
+        g_vel_each = reg[("PartType0", "particle_velocity")].in_units("km/s").v.tolist()
+    
+    if code_name == 'GIZMO':
+        ds = yt.load(pfs[int(idx)]) #GIZMO automatically includes the correct conversion factor
 
         coor = tree['0'][idx]['coor']
         rvir = tree['0'][idx]['Rvir']
@@ -99,9 +131,9 @@ for sto, idx in yt.parallel_objects(snapshot_idx, nprocs-1,storage = my_storage)
     bary_angmoment_each = np.cross(bary_relative_coor_each,bary_relative_momentum_each)
 
     output_star = {'com_coor_bary':com_coor_bary,'com_coor_star':com_coor_star,'com_vel_bary':com_vel_bary,'com_vel_star':com_vel_star,'mass':s_mass_each,'coor':s_coor_each,'vel':s_vel_each}
-    output_gas = {'com_coor_bary':com_coor_bary,'com_vel_bary':com_vel_bary,'mass':g_mass_each,'coor':g_coor_each,'vel':g_vel_each}
+    #output_gas = {'com_coor_bary':com_coor_bary,'com_vel_bary':com_vel_bary,'mass':g_mass_each,'coor':g_coor_each,'vel':g_vel_each}
     output_bary = {'com_coor_bary':com_coor_bary,'com_vel_bary':com_vel_bary,'mass':bary_mass_each,'rel_coor':bary_relative_coor_each,'rel_vel':bary_relative_vel_each,'rel_momentum':bary_relative_momentum_each,'angmoment':bary_angmoment_each}
 
     np.save('metadata/stars_%s.npy' % idx,output_star)
-    np.save('metadata/gas_%s.npy' % idx,output_gas)
+    #np.save('metadata/gas_%s.npy' % idx,output_gas)
     np.save('metadata/bary_%s.npy' % idx,output_bary)
