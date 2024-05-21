8750fb1b805b3dd8ebdf710cb93c8df9095a6b9c
thinhhn2 <thinhnh3009@gmail.com>
2024-05-21 18:06:55 +0700

    Parallelization

--- plot_mass_distribution_and_dip_test.py
+++ plot_mass_distribution_and_dip_test.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 20 11:48:13 2024
-
-@author: thinhnguyen
-"""
 import numpy as np
 import yt
 import matplotlib.pyplot as plt
@@ -14,6 +7,12 @@ import astropy.units as u
 from yt.data_objects.particle_filters import add_particle_filter
 from yt.data_objects.unions import ParticleUnion
 from mpl_toolkits.axes_grid1 import AxesGrid
+
+yt.enable_parallelism()
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.rank
+nprocs = comm.size
 
 def univDen(ds):
     # Hubble constant
@@ -49,152 +48,161 @@ pfs = np.loadtxt('pfs_manual.dat',dtype=str)
 codetp = 'ENZO'
 
 branch_idx = '0'
-snapshot_idx = 100
-halo = tree[branch_idx][str(snapshot_idx)]
 
-if codetp == 'GADGET3' or codetp == 'AREPO':
-    ds = yt.load(pfs[snapshot_idx], unit_base = {"length": (1.0, "Mpccm/h")})
-else: 
-    ds = yt.load(pfs[snapshot_idx])
+my_storage= {}
 
-com = halo['new_center']
-rvir = halo['Rvir']
-reg = ds.sphere(com,(rvir,'code_length'))
-
-#Finding R500 using gas + stars particles
-if codetp == 'ENZO':
-    def stars(pfilter, data):
-         filter_stars = data["all", "particle_type"] == 2
-         return filter_stars
-    add_particle_filter("stars", function=stars, filtered_type="all", requires=["particle_type"])
-    ds.add_particle_filter("stars")
-    pos_star = reg["stars","particle_position"].in_units('m').v
-    mass_star = reg["stars","particle_mass"].in_units('kg').v
-    x_gas = reg['gas','x'].to('m').v
-    y_gas = reg['gas','y'].to('m').v
-    z_gas = reg['gas','z'].to('m').v
-    pos_gas = np.array([x_gas,y_gas,z_gas]).T
-    mass_gas = reg['gas','cell_mass'].to('kg').v
-    pos = np.vstack((pos_star,pos_gas))
-    mass = np.append(mass_star,mass_gas)
-elif codetp == 'GEAR':
-    nm = ParticleUnion("NormalMatter",["PartType1","PartType0"])
-    ds.add_particle_union(nm)
-    pos = reg['NormalMatter','particle_position'].to('m').v
-    mass = reg['NormalMatter','particle_mass'].to('kg').v
-elif codetp == 'GADGET3' or codetp == 'AREPO' or codetp == 'GIZMO':
-    nm = ParticleUnion("NormalMatter",["PartType4","PartType0"])
-    ds.add_particle_union(nm)
-    pos = reg['NormalMatter','particle_position'].to('m').v
-    mass = reg['NormalMatter','particle_mass'].to('kg').v
-elif codetp == 'ART':
-    pos_star = reg["stars","particle_position"].in_units('m').v
-    mass_star = reg["stars","particle_mass"].in_units('kg').v
-    x_gas = reg['gas','x'].to('m').v
-    y_gas = reg['gas','y'].to('m').v
-    z_gas = reg['gas','z'].to('m').v
-    pos_gas = np.array([x_gas,y_gas,z_gas]).T
-    mass_gas = reg['gas','cell_mass'].to('kg').v
-    pos = np.vstack((pos_star,pos_gas))
-    mass = np.append(mass_star,mass_gas)
-elif codetp == 'RAMSES':
-    pos_star = reg["star","particle_position"].in_units('m').v
-    mass_star = reg["star","particle_mass"].in_units('kg').v
-    x_gas = reg['gas','x'].to('m').v
-    y_gas = reg['gas','y'].to('m').v
-    z_gas = reg['gas','z'].to('m').v
-    pos_gas = np.array([x_gas,y_gas,z_gas]).T
-    mass_gas = reg['gas','cell_mass'].to('kg').v
-    pos = np.vstack((pos_star,pos_gas))
-    mass = np.append(mass_star,mass_gas)
-elif codetp == 'CHANGA':
-    nm = ParticleUnion("NormalMatter",["Stars","Gas"])
-    ds.add_particle_union(nm)
-    pos = reg['NormalMatter','particle_position'].to('m').v
-    mass = reg['NormalMatter','particle_mass'].to('kg').v
+for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, storage = my_storage):
+    halo = tree[branch_idx][snapshot_idx]
     
-
-#data = np.load('stars_149_branch-0.npy',allow_pickle=True).tolist()
-
-#com = np.array([8276.79690708, 8695.9163787 , 8435.20267753])
-#radius_s_g_500 = 11.44856141 #kpc
-
-#pos = np.array(data['coor'])
-#mass = np.array(data['mass'])
-
-com_m = (com*ds.units.code_length).in_units('m').v
-com_kpc = (com*ds.units.code_length).in_units('kpc').v
-radius500 ,cden = Find_virRad(com_m,pos,mass,ds,oden=500,radmax=1e50) #R500 using only normal matter
-radius500_kpc = (radius500*ds.units.m).in_units('kpc').v.tolist()
-rvir_kpc = (rvir*ds.units.code_length).in_units('kpc').v.tolist()
-
-#Plotting the surface mass density in 3 axis
-fig = plt.figure()
-
-grid = AxesGrid(
-    fig,
-    (0.075, 0.075, 0.85, 0.85),
-    nrows_ncols=(1, 3),
-    axes_pad=1.0,
-    label_mode="all",
-    share_all=True,
-    cbar_location="right",
-    cbar_mode="edge",
-    cbar_size="5%",
-    cbar_pad="0%",
-)
-for i in range(3):
-    # Load the data and create a single plot
-    p = yt.ParticleProjectionPlot(ds, i, [("stars","particle_mass")], center = (com_kpc,'kpc'), width = (2*rvir_kpc, 'kpc'),density=True,fontsize=12)
-    p.annotate_sphere(com.v.tolist(), radius=(radius500_kpc, "kpc"), circle_args={"color": "black"})
+    if codetp == 'GADGET3' or codetp == 'AREPO':
+        ds = yt.load(pfs[int(snapshot_idx)], unit_base = {"length": (1.0, "Mpccm/h")})
+    else: 
+        ds = yt.load(pfs[int(snapshot_idx)])
+    
+    com = halo['new_center']
+    rvir = halo['Rvir']
+    reg = ds.sphere(com,(rvir,'code_length'))
+    
+    #Finding R500 using gas + stars particles
+    if codetp == 'ENZO':
+        def stars(pfilter, data):
+             filter_stars = data["all", "particle_type"] == 2
+             return filter_stars
+        add_particle_filter("stars", function=stars, filtered_type="all", requires=["particle_type"])
+        ds.add_particle_filter("stars")
+        pos_star = reg["stars","particle_position"].in_units('m').v
+        mass_star = reg["stars","particle_mass"].in_units('kg').v
+        x_gas = reg['gas','x'].to('m').v
+        y_gas = reg['gas','y'].to('m').v
+        z_gas = reg['gas','z'].to('m').v
+        pos_gas = np.array([x_gas,y_gas,z_gas]).T
+        mass_gas = reg['gas','cell_mass'].to('kg').v
+        pos = np.vstack((pos_star,pos_gas))
+        mass = np.append(mass_star,mass_gas)
+    elif codetp == 'GEAR':
+        nm = ParticleUnion("NormalMatter",["PartType1","PartType0"])
+        ds.add_particle_union(nm)
+        pos = reg['NormalMatter','particle_position'].to('m').v
+        mass = reg['NormalMatter','particle_mass'].to('kg').v
+    elif codetp == 'GADGET3' or codetp == 'AREPO' or codetp == 'GIZMO':
+        nm = ParticleUnion("NormalMatter",["PartType4","PartType0"])
+        ds.add_particle_union(nm)
+        pos = reg['NormalMatter','particle_position'].to('m').v
+        mass = reg['NormalMatter','particle_mass'].to('kg').v
+    elif codetp == 'ART':
+        pos_star = reg["stars","particle_position"].in_units('m').v
+        mass_star = reg["stars","particle_mass"].in_units('kg').v
+        x_gas = reg['gas','x'].to('m').v
+        y_gas = reg['gas','y'].to('m').v
+        z_gas = reg['gas','z'].to('m').v
+        pos_gas = np.array([x_gas,y_gas,z_gas]).T
+        mass_gas = reg['gas','cell_mass'].to('kg').v
+        pos = np.vstack((pos_star,pos_gas))
+        mass = np.append(mass_star,mass_gas)
+    elif codetp == 'RAMSES':
+        pos_star = reg["star","particle_position"].in_units('m').v
+        mass_star = reg["star","particle_mass"].in_units('kg').v
+        x_gas = reg['gas','x'].to('m').v
+        y_gas = reg['gas','y'].to('m').v
+        z_gas = reg['gas','z'].to('m').v
+        pos_gas = np.array([x_gas,y_gas,z_gas]).T
+        mass_gas = reg['gas','cell_mass'].to('kg').v
+        pos = np.vstack((pos_star,pos_gas))
+        mass = np.append(mass_star,mass_gas)
+    elif codetp == 'CHANGA':
+        nm = ParticleUnion("NormalMatter",["Stars","Gas"])
+        ds.add_particle_union(nm)
+        pos = reg['NormalMatter','particle_position'].to('m').v
+        mass = reg['NormalMatter','particle_mass'].to('kg').v
+        
+    
+    #data = np.load('stars_149_branch-0.npy',allow_pickle=True).tolist()
+    
+    #com = np.array([8276.79690708, 8695.9163787 , 8435.20267753])
+    #radius_s_g_500 = 11.44856141 #kpc
+    
+    #pos = np.array(data['coor'])
+    #mass = np.array(data['mass'])
+    
+    com_m = (com*ds.units.code_length).in_units('m').v
+    com_kpc = (com*ds.units.code_length).in_units('kpc').v
+    radius500 ,cden = Find_virRad(com_m,pos,mass,ds,oden=500,radmax=1e50) #R500 using only normal matter
+    radius500_kpc = (radius500*ds.units.m).in_units('kpc').v.tolist()
+    rvir_kpc = (rvir*ds.units.code_length).in_units('kpc').v.tolist()
+    
+    #Plotting the surface mass density in 3 axis
+    fig = plt.figure()
+    
+    grid = AxesGrid(
+        fig,
+        (0.075, 0.075, 0.85, 0.85),
+        nrows_ncols=(1, 3),
+        axes_pad=1.0,
+        label_mode="all",
+        share_all=True,
+        cbar_location="right",
+        cbar_mode="edge",
+        cbar_size="5%",
+        cbar_pad="0%",
+    )
+    
+    for i in range(3):
+        # Load the data and create a single plot
+        p = yt.ParticleProjectionPlot(ds, i, [("stars","particle_mass")], center = (com,'code_length'), width = (2*rvir_kpc, 'kpc'),density=True,fontsize=12)
+        p.annotate_sphere(com.tolist(), radius=(radius500_kpc, "kpc"), circle_args={"color": "black"})
+        p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
+        p.set_colorbar_label(("stars", "particle_mass"),r'$\Sigma_{*}$ $(\mathrm{M}_\odot/\mathrm{kpc}^{2})$')
+        p.plots[('stars', 'particle_mass')].figure = fig
+        p.plots[('stars', 'particle_mass')].axes = grid[i].axes
+        p.plots[('stars', 'particle_mass')].cax = grid.cbar_axes[i]
+        # Finally, this actually redraws the plot.
+        p.render()
+    
+    plt.savefig("surface_star_density_plots/surface_mass_density_%s_%s.png" % (branch_idx, snapshot_idx),dpi=600,bbox_inches='tight')
+    
+    
+    """
+    p = yt.ParticleProjectionPlot(ds,normal=2,fields=[("stars", "particle_mass")],center=(com_kpc,'kpc'),width=(rvir_kpc,'kpc'),density=True)
     p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
-    p.set_colorbar_label(("stars", "particle_mass"),r'$\Sigma_{*}$ $(\mathrm{M}_\odot/\mathrm{kpc}^{2})$')
-    p.plots[('stars', 'particle_mass')].figure = fig
-    p.plots[('stars', 'particle_mass')].axes = grid[i].axes
-    p.plots[('stars', 'particle_mass')].cax = grid.cbar_axes[i]
-    # Finally, this actually redraws the plot.
-    p.render()
-
-plt.savefig("surface_mass_density_%s_%s.png" % (branch_idx, snapshot_idx),dpi=600,bbox_inches='tight')
-
-
-"""
-p = yt.ParticleProjectionPlot(ds,normal=2,fields=[("stars", "particle_mass")],center=(com_kpc,'kpc'),width=(rvir_kpc,'kpc'),density=True)
-p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
-
-fig, ax = plt.subplots(figsize=(9,9))
-p = yt.ParticleProjectionPlot(ds,normal=2,fields=[("stars", "particle_mass")],center=(com_kpc,'kpc'),width=(rvir_kpc,'kpc'),density=True)
-p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
-plt.scatter(pos[:,0],pos[:,1],s=1)
-plt.scatter(com[0], com[1], s = 20, color='red')
-circle = plt.Circle((com[0], com[1]), radius500, color='red', fill=False)
-ax.add_patch(circle)
-ax.set_xlabel('x (kpc)',fontsize=14)
-ax.set_ylabel(r'y (kpc)',fontsize=14)
-ax.tick_params(axis='x', labelsize=14)
-ax.tick_params(axis='y', labelsize=14)
-"""
-
-r = np.linalg.norm(pos - com_m, axis = 1)
-mass_gal = mass[r < radius500]
-r_gal = r[r < radius500]
-
-dip, pval = diptest.diptest(r_gal*mass_gal)
-if pval < 0.05:
-    print('This is a multimodal distribution')
-
-#plt.figure()
-#plt.hist(r_gal,weights=mass_gal, bins=75)
-
-data = {'r':r_gal, 'mass':mass_gal}
-
-plt.figure(figsize=(9,6))
-ax = sns.histplot(data=data, x = 'r', weights='mass', bins=75, kde=True, alpha=0.2, color='tab:blue',kde_kws={'gridsize':500,'bw_adjust':0.4})
-ax.lines[0].set_color('blue')
-#The reason the KDE fit cannot fit the first peak well is because it tries to fit a Gaussian model
-ax.set_xlabel('r (kpc)',fontsize=14)
-ax.set_ylabel(r'mass ($M_\odot$)',fontsize=14)
-ax.tick_params(axis='x', labelsize=14)
-ax.tick_params(axis='y', labelsize=14)
-ax.yaxis.get_offset_text().set_fontsize(14)
-ax.set_title('Hartigan\'s dip test p-value: %.3f' % pval, fontsize=15)
+    
+    fig, ax = plt.subplots(figsize=(9,9))
+    p = yt.ParticleProjectionPlot(ds,normal=2,fields=[("stars", "particle_mass")],center=(com_kpc,'kpc'),width=(rvir_kpc,'kpc'),density=True)
+    p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
+    plt.scatter(pos[:,0],pos[:,1],s=1)
+    plt.scatter(com[0], com[1], s = 20, color='red')
+    circle = plt.Circle((com[0], com[1]), radius500, color='red', fill=False)
+    ax.add_patch(circle)
+    ax.set_xlabel('x (kpc)',fontsize=14)
+    ax.set_ylabel(r'y (kpc)',fontsize=14)
+    ax.tick_params(axis='x', labelsize=14)
+    ax.tick_params(axis='y', labelsize=14)
+    """
+    
+    r = np.linalg.norm(pos - com_m, axis = 1)
+    mass_gal = mass[r < radius500]
+    r_gal = r[r < radius500]
+    
+    dip, pval = diptest.diptest(r_gal*mass_gal)
+    if pval <= 0.05:
+        print('This is a multimodal distribution')
+    else:
+        print('This is a unimodal distribution')
+    
+    #plt.figure()
+    #plt.hist(r_gal,weights=mass_gal, bins=75)
+    
+    mass_gal_Msun = (mass_gal*ds.units.kg).to('Msun').v
+    r_gal_kpc = (r_gal*ds.units.m).to('kpc').v
+    data = {'r':r_gal_kpc, 'mass':mass_gal_Msun}
+    
+    plt.figure(figsize=(9,6))
+    ax = sns.histplot(data=data, x = 'r', weights='mass', bins=75, kde=True, alpha=0.2, color='tab:blue',kde_kws={'gridsize':500,'bw_adjust':0.4})
+    ax.lines[0].set_color('blue')
+    #The reason the KDE fit cannot fit the first peak well is because it tries to fit a Gaussian model
+    ax.set_xlabel('r (kpc)',fontsize=14)
+    ax.set_ylabel(r'mass ($M_\odot$)',fontsize=14)
+    ax.tick_params(axis='x', labelsize=14)
+    ax.tick_params(axis='y', labelsize=14)
+    ax.yaxis.get_offset_text().set_fontsize(14)
+    ax.set_title('Hartigan\'s dip test p-value: %.3f' % pval, fontsize=15)
+    plt.savefig('mass_distribution_plots/radial_mass_distribution_%s_%s.png' % (branch_idx, snapshot_idx), dpi=600, bbox_inches='tight')
