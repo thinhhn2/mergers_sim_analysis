7f6e79fcd3029b3a4d635d04cd108e7fd5741ec2
thinhhn2 <thinhnh3009@gmail.com>
2024-05-29 10:22:30 +0700

    Integrate with the center_of_mass_buildingUp method

--- plot_mass_distribution_and_dip_test.py
+++ plot_mass_distribution_and_dip_test.py
@@ -43,7 +43,40 @@ def Find_virRad(com,pos,mass,ds,oden=200,radmax=1e50):
         cden = 0
     return radius, cden
 
-tree = np.load('halotree_Thinh_structure_with_com_0-15Rvir.npy',allow_pickle=True).tolist()
+def Find_Com_and_virRad(initial_gal_com, halo_rvir, pos, mass, oden=500):
+    #Make sure the unit is in kg and m
+    virRad = 0.01*halo_rvir
+    com = initial_gal_com
+    fden = np.inf
+    while fden > oden:
+        r = np.linalg.norm(pos - com, axis=1)
+        pos_in = pos[r < virRad]
+        mass_in = mass[r < virRad]
+        den = np.sum(mass_in) / (4/3 * np.pi * virRad**3)
+        uden = univDen(ds)
+        fden = den/uden
+        com = np.average(pos_in, weights=mass_in, axis=0)
+        virRad += 0.005*halo_rvir
+    return com, virRad
+
+def Find_Com_HighestDensity(ds, reg, deposit_dim = 5):
+    halo_com = reg.center.to('code_length').v
+    halo_rvir = reg.radius.to('code_length').v
+    #Divide the region into grids to quickly find the maximum stellar density coordinate
+    reg_grid = ds.arbitrary_grid(halo_com - halo_rvir, halo_com + halo_rvir ,dims=[deposit_dim,deposit_dim,deposit_dim])
+    density_grid = reg_grid['deposit',star_name_dict[codetp]+'_density'].to('Msun/kpc**3').v
+    density_grid_flat = density_grid.flatten()
+    #Find the index of maximum density
+    maxdensity_idx = np.unravel_index(np.argmax(density_grid_flat), density_grid.shape)
+    #Obtain the coordinates of the maximum density
+    x_grid = reg_grid['x'].to('code_length').v
+    y_grid = reg_grid['y'].to('code_length').v
+    z_grid = reg_grid['z'].to('code_length').v
+    center_grid = np.stack((x_grid,y_grid,z_grid),axis=3)
+    initial_gal_com = center_grid[maxdensity_idx[0],maxdensity_idx[1],maxdensity_idx[2],:]
+    return initial_gal_com
+
+tree = np.load('halotree_Thinh_structure_with_com.npy',allow_pickle=True).tolist()
 pfs = np.loadtxt('pfs_manual.dat',dtype=str)
 codetp = 'ENZO'
 
@@ -59,11 +92,11 @@ for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, 
     else: 
         ds = yt.load(pfs[int(snapshot_idx)])
     
-    com = halo['new_center']
-    rvir = halo['Rvir']
-    reg = ds.sphere(com,(rvir,'code_length'))
+    halo_com = halo['coor']
+    halo_rvir = halo['Rvir']
+    reg = ds.sphere(halo_com,(halo_rvir,'code_length'))
     
-    #Finding R500 using gas + stars particles
+    #Finding R2000 using gas + stars particles
     if codetp == 'ENZO':
         def stars(pfilter, data):
              filter_stars = data["all", "particle_type"] == 2
@@ -114,21 +147,34 @@ for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, 
         ds.add_particle_union(nm)
         pos = reg['NormalMatter','particle_position'].to('m').v
         mass = reg['NormalMatter','particle_mass'].to('kg').v
-        
     
-    #data = np.load('stars_149_branch-0.npy',allow_pickle=True).tolist()
-    
-    #com = np.array([8276.79690708, 8695.9163787 , 8435.20267753])
-    #radius_s_g_500 = 11.44856141 #kpc
-    
-    #pos = np.array(data['coor'])
-    #mass = np.array(data['mass'])
-    
-    com_m = (com*ds.units.code_length).in_units('m').v
-    com_kpc = (com*ds.units.code_length).in_units('kpc').v
-    radius500 ,cden = Find_virRad(com_m,pos,mass,ds,oden=500,radmax=1e50) #R500 using only normal matter
-    radius500_kpc = (radius500*ds.units.m).in_units('kpc').v.tolist()
-    rvir_kpc = (rvir*ds.units.code_length).in_units('kpc').v.tolist()
+    if snapshot_idx == 143 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49442,0.51790,0.50258])
+    elif snapshot_idx == 144 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49445,0.51805,0.50263])
+    elif snapshot_idx == 145 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49439,0.51815,0.50270])
+    elif snapshot_idx == 146 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49429,0.51840,0.50277])
+    elif snapshot_idx == 147 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49419,0.51855,0.50285])
+    elif snapshot_idx == 148 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49405,0.51869,0.50297])
+    elif snapshot_idx == 149 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49393,0.51885,0.50312])
+    elif snapshot_idx == 150 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49380,0.51900,0.50332])
+    elif snapshot_idx == 151 and codetp == 'ENZO':
+        initial_gal_com = np.array([0.49372,0.51917,0.50339])
+    else:
+        initial_gal_com = Find_Com_HighestDensity(ds, reg)
+
+    initial_gal_com_m = (initial_gal_com*ds.units.code_length).in_units('m').v
+    initial_gal_com_kpc = (initial_gal_com*ds.units.code_length).in_units('kpc').v
+    gal_com_m, gal_r2000_m = Find_Com_and_virRad(initial_gal_com_m, halo_rvir, pos, mass, oden=2000) #R2000 using star and gas
+    gal_com = (gal_com_m*ds.units.m).in_units('code_length').v
+    gal_r2000_kpc = (gal_r2000_m*ds.units.m).in_units('kpc').v
+    halo_rvir_kpc = (halo_rvir*ds.units.code_length).in_units('kpc').v.tolist()
     
     #Plotting the surface mass density in 3 axis
     fig = plt.figure()
@@ -148,19 +194,19 @@ for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, 
     
     for i in range(3):
         # Load the data and create a single plot
-        p = yt.ParticleProjectionPlot(ds, i, [("stars","particle_mass")], center = (com,'code_length'), width = (2*rvir_kpc, 'kpc'),density=True,fontsize=12)
-        p.annotate_sphere(com.tolist(), radius=(radius500_kpc, "kpc"), circle_args={"color": "black"})
+        p = yt.ParticleProjectionPlot(ds, i, [("stars","particle_mass")], center = (gal_com,'code_length'), width = (2*halo_rvir_kpc, 'kpc'),density=True,fontsize=12)
+        p.annotate_sphere(gal_com.tolist(), radius=(gal_r2000_kpc, "kpc"), circle_args={"color": "black"})
         p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
         p.set_colorbar_label(("stars", "particle_mass"),r'$\Sigma_{*}$ $(\mathrm{M}_\odot/\mathrm{kpc}^{2})$')
         if i == 1:
-            p.annotate_title('R500 = %.2f Rvir' % radius500_kpc/rvir_kpc)
+            p.annotate_title('R2000 = %.2f Rvir' % gal_r2000_kpc/halo_rvir_kpc)
         p.plots[('stars', 'particle_mass')].figure = fig
         p.plots[('stars', 'particle_mass')].axes = grid[i].axes
         p.plots[('stars', 'particle_mass')].cax = grid.cbar_axes[i]
         # Finally, this actually redraws the plot.
         p.render()
     
-    plt.savefig("surface_star_density_plots/surface_mass_density_%s_%s.png" % (branch_idx, snapshot_idx),dpi=600,bbox_inches='tight')
+    plt.savefig("surface_star_density_plots_2/surface_mass_density_%s_%s.png" % (branch_idx, snapshot_idx),dpi=600,bbox_inches='tight')
     
     
     """
@@ -180,9 +226,9 @@ for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, 
     ax.tick_params(axis='y', labelsize=14)
     """
     
-    r = np.linalg.norm(pos - com_m, axis = 1)
-    mass_gal = mass[r < radius500]
-    r_gal = r[r < radius500]
+    r = np.linalg.norm(pos - gal_com_m, axis = 1)
+    mass_gal = mass[r < gal_r2000_m]
+    r_gal = r[r < gal_r2000_m]
     
     dip, pval = diptest.diptest(r_gal*mass_gal)
     if pval <= 0.05:
@@ -207,4 +253,4 @@ for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, 
     ax.tick_params(axis='y', labelsize=14)
     ax.yaxis.get_offset_text().set_fontsize(14)
     ax.set_title('Hartigan\'s dip test p-value: %.3f' % pval, fontsize=15)
-    plt.savefig('mass_distribution_plots/radial_mass_distribution_%s_%s.png' % (branch_idx, snapshot_idx), dpi=600, bbox_inches='tight')
+    plt.savefig('mass_distribution_plots_2/radial_mass_distribution_%s_%s.png' % (branch_idx, snapshot_idx), dpi=600, bbox_inches='tight')
