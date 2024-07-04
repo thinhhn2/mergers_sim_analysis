2e5ea5b7479f193a2934ea1671240a0dd0dcbd0b
thinhhn2 <thinhnh3009@gmail.com>
2024-07-04 10:01:10 +0700

    Make the particle plot manually with matplotlib

--- plot_mass_distribution_and_dip_test.py
+++ plot_mass_distribution_and_dip_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import yt
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 import diptest
 import astropy.units as u 
@@ -79,10 +80,19 @@ def Find_Com_HighestDensity(ds, reg, deposit_dim = 5):
     initial_gal_com = center_grid[maxdensity_idx[0],maxdensity_idx[1],maxdensity_idx[2],:]
     return initial_gal_com
 
-os.chdir('/scratch/bbvl/tnguyen2/ENZO')
-tree = np.load('/scratch/bbvl/tnguyen2/ENZO/halotree_Thinh_structure_with_com.npy',allow_pickle=True).tolist()
-pfs = np.loadtxt('/scratch/bbvl/tnguyen2/ENZO/pfs_manual.dat',dtype=str)
 codetp = 'ENZO'
+os.chdir('/scratch/bbvl/tnguyen2/%s' % codetp)
+if not os.path.exists('/scratch/bbvl/tnguyen2/%s/radius_2000' % codetp):
+    os.mkdir('/scratch/bbvl/tnguyen2/%s/radius_2000' % codetp)
+
+if codetp == 'ENZO':
+    tree = np.load('/scratch/bbvl/tnguyen2/ENZO/halotree_Thinh_structure_with_com.npy',allow_pickle=True).tolist()
+elif codetp == 'AREPO':
+    tree = np.load('/scratch/bbvl/tnguyen2/ENZO/halotree_Thinh_structure_refinedboundary_2nd_manual_add_2nd_merger.npy',allow_pickle=True).tolist()
+else:
+    tree = np.load('/scratch/bbvl/tnguyen2/%s/halotree_Thinh_structure_refinedboundary_2nd.npy' % codetp, allow_pickle=True).tolist()
+
+pfs = np.loadtxt('/scratch/bbvl/tnguyen2/%s/pfs_manual.dat' % codetp, dtype=str)
 
 branch_idx = '0'
 
@@ -182,14 +192,60 @@ for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, 
     halo_rvir_kpc = (halo_rvir*ds.units.code_length).in_units('kpc').v.tolist()
     gal_com_m, gal_r2000_m = Find_Com_and_virRad(initial_gal_com_m, halo_rvir_m, pos, mass, oden=2000) #R2000 using star and gas
     gal_com = (gal_com_m*ds.units.m).in_units('code_length').v
-    gal_r2000_kpc = (gal_r2000_m*ds.units.m).in_units('kpc').v
-    gal_r2000 = (gal_r2000_m*ds.units.m).in_units('code_length').v
+    gal_r2000_kpc = (gal_r2000_m*ds.units.m).in_units('kpc').v.tolist()
+    gal_r2000 = (gal_r2000_m*ds.units.m).in_units('code_length').v.tolist()
     #Export the data
     output = {}
     output['gal_com'] = gal_com
     output['gal_r2000'] = gal_r2000
     np.save('radius_2000/gal_com_r2000_SnapIdx_%s.npy' % snapshot_idx, output)
     #Plotting the surface mass density in 3 axis
+    plt.figure(figsize=(16,5))
+    #
+    rel_pos_star = pos_star - gal_com_m
+    rel_pos_star_kpc = (rel_pos_star*ds.units.m).to('kpc').v
+    rel_x = rel_pos_star_kpc[:,0]
+    rel_y = rel_pos_star_kpc[:,1]
+    rel_z = rel_pos_star_kpc[:,2]
+    #
+    ax1 = plt.subplot(1,3,1)
+    _ = ax1.hist2d(rel_x, rel_y, bins=800, norm=mpl.colors.LogNorm(), cmap=mpl.cm.viridis)
+    circle1 = plt.Circle((0, 0), gal_r2000_kpc, fill=False)
+    ax1.add_patch(circle1)
+    ax1.set_xlabel('x (kpc)', fontsize=16)
+    ax1.set_ylabel('y (kpc)', fontsize=16)
+    ax1.tick_params(axis='x', labelsize=16)
+    ax1.tick_params(axis='y', labelsize=16)
+    ax1.set_xlim( -np.max(abs(rel_x)), np.max(abs(rel_x)))
+    ax1.set_ylim( -np.max(abs(rel_y)), np.max(abs(rel_y)))
+    #
+    ax2 = plt.subplot(1,3,2)
+    _ = ax2.hist2d(rel_y, rel_z, bins=800, norm=mpl.colors.LogNorm(), cmap=mpl.cm.viridis)
+    circle2 = plt.Circle((0, 0), gal_r2000_kpc, fill=False)
+    ax2.add_patch(circle2)
+    ax2.set_xlabel('y (kpc)', fontsize=16)
+    ax2.set_ylabel('z (kpc)', fontsize=16)
+    ax2.tick_params(axis='x', labelsize=16)
+    ax2.tick_params(axis='y', labelsize=16)
+    ax2.set_xlim( -np.max(abs(rel_y)), np.max(abs(rel_y)))
+    ax2.set_ylim( -np.max(abs(rel_z)), np.max(abs(rel_z)))
+    ax2.set_title(r'$r_{baryon,2000}$ = %.2f kpc = %.2f Rvir' % (gal_r2000_kpc, gal_r2000_kpc/halo_rvir_kpc), fontsize=16)
+    #
+    ax3 = plt.subplot(1,3,3)
+    _ = ax3.hist2d(rel_z, rel_x, bins=800, norm=mpl.colors.LogNorm(), cmap=mpl.cm.viridis)
+    circle3 = plt.Circle((0, 0), gal_r2000_kpc, fill=False)
+    ax3.add_patch(circle3)
+    ax3.set_xlabel('z (kpc)', fontsize=16)
+    ax3.set_ylabel('x (kpc)', fontsize=16)
+    ax3.tick_params(axis='x', labelsize=16)
+    ax3.tick_params(axis='y', labelsize=16)
+    ax3.set_xlim( -np.max(abs(rel_z)), np.max(abs(rel_z)))
+    ax3.set_ylim( -np.max(abs(rel_x)), np.max(abs(rel_x)))
+    #
+    plt.tight_layout()
+    plt.savefig("radius_2000/surface_mass_density_%s_%s.png" % (branch_idx, snapshot_idx),dpi=300,bbox_inches='tight')    
+    """
+    #using yt to plot, but this method is slow
     fig = plt.figure()
     grid = AxesGrid(
         fig,
@@ -218,23 +274,7 @@ for sto, snapshot_idx in yt.parallel_objects(tree[branch_idx].keys(), nprocs-1, 
         # Finally, this actually redraws the plot.
         p.render()
     #
-    plt.savefig("radius_2000/surface_mass_density_%s_%s.png" % (branch_idx, snapshot_idx),dpi=600,bbox_inches='tight')
-    
-    """
-    p = yt.ParticleProjectionPlot(ds,normal=2,fields=[("stars", "particle_mass")],center=(com_kpc,'kpc'),width=(rvir_kpc,'kpc'),density=True)
-    p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
-    
-    fig, ax = plt.subplots(figsize=(9,9))
-    p = yt.ParticleProjectionPlot(ds,normal=2,fields=[("stars", "particle_mass")],center=(com_kpc,'kpc'),width=(rvir_kpc,'kpc'),density=True)
-    p.set_unit(("stars", "particle_mass"), "Msun/kpc**2")
-    plt.scatter(pos[:,0],pos[:,1],s=1)
-    plt.scatter(com[0], com[1], s = 20, color='red')
-    circle = plt.Circle((com[0], com[1]), radius500, color='red', fill=False)
-    ax.add_patch(circle)
-    ax.set_xlabel('x (kpc)',fontsize=14)
-    ax.set_ylabel(r'y (kpc)',fontsize=14)
-    ax.tick_params(axis='x', labelsize=14)
-    ax.tick_params(axis='y', labelsize=14)
+    plt.savefig("radius_2000/surface_mass_density_%s_%s.png" % (branch_idx, snapshot_idx),dpi=300,bbox_inches='tight')
     """
     """
     #Make the radial mass distribution plot
